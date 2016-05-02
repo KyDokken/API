188499ab39c6363829fd483de49ebde752fbb9b5
KyDokken <kyle.r.dokken@gmail.com>
2016-05-02 09:01:16 -0600

    v2

--- apps/app.js
+++ apps/app.js
@@ -1,6 +1,14 @@
-$(doucment).ready(function () {});
+$(document).ready(function () {});
+
 $(function () {
     $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function (data) {
-        console.log(data);
+        showResults(data.Search);
     });
 });
+
+// Seperate the GET DATA part of the app from the SHOW DATA part of the APP ^^^^ becomes >>>>
+
+function showResults(results) {
+    $.each(results, function (index, value) {
+        console.log(value.Title);
+    });
